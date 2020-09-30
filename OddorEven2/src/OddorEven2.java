d6101470fe12081dd4380db497d57f976d91a022
Shivam Yadav <sy7052789@gmail.com>
2020-09-30 18:44:54 +0530

    Added comments on each line
    That's a good practice to have a comment before each valuable line.

--- OddorEven2/src/OddorEven2.java
+++ OddorEven2/src/OddorEven2.java
@@ -1,22 +1,26 @@
+package ODDorEVEN.OddorEven2.src;
 
 import java.util.Scanner;
 
 public class OddorEven2 {
 	public static void main(String [] args)
 	{
+		// Declaring input using scanner *must include Scanner import*
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter number");
+		System.out.println("Enter Any Integer Value");
+		
+		// Entering an integer value using next int function in Scanner class
 		int number = keyboard.nextInt();
-		if(number%2== 0)
+
+		// checking if any number (let number variable) is divisible by 2 or not
+		if(number % 2 == 0)
 		{
 			System.out.println("That's an EVEN number");
 		}
+		
+		// If not then it is obviously an Odd number
 		else {
 			System.out.println("That's an ODD number");
-		}
-		
-		
+		}	
 	}
-	
-
 }
